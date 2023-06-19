67920c80f3142ca1999a4c5e2a10fc6526f09788
navas <gabrielnavas@email.com>
2023-06-19 19:51:59 -0300

    refactorin main urls to recipes urls

---
+++ recipes/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from recipes.views import home, about, contact
+
+
+urlpatterns = [
+    path('', home),
+    path('about/', about),
+    path('contact/', contact),
+]
