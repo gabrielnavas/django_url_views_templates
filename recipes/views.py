71ef6ab3d78d9bbbb26cce02d887aac668e4fbc8
navas <gabrielnavas@email.com>
2023-06-19 20:25:01 -0300

    add path global and path recipes inside recipes app

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
 def home(request: HttpRequest) -> HttpResponse:
-    return render(request, 'home.html')
+    return render(request, 'recipes/home.html')
 
 def about(request: HttpRequest) -> HttpResponse:
     return HttpResponse('about page')
