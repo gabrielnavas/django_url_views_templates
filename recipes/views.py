b9b115b512133a73c7776f31d5f15d3d8b9f995f
navas <gabrielnavas@email.com>
2023-06-21 19:42:00 -0300

    add recipe list item view

--- recipes/views.py
+++ recipes/views.py
@@ -11,4 +11,4 @@ def recipe(request: HttpRequest, id) -> HttpResponse:
     context = {
         'name': 'Gabriel'
     }
-    return render(request, 'recipes/pages/home.html', context, status=200)
+    return render(request, 'recipes/pages/recipe-view.html', context, status=200)
