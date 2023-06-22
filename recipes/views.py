75a4e879a6b0b527cce8c4af6a278ed51b25622d
navas <gabrielnavas@email.com>
2023-06-21 21:22:45 -0300

    add random data recupes on list and item list pages

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
+from utils.recipes.factory import make_recipe
+
 def home(request: HttpRequest) -> HttpResponse:
+    max_recipes_fake = 11
     context = {
-        'name': 'Gabriel'
+        'recipes': [make_recipe() for _ in range(max_recipes_fake)],
     }
     return render(request, 'recipes/pages/home.html', context, status=200)
 
 def recipe(request: HttpRequest, id) -> HttpResponse:
     context = {
-        'name': 'Gabriel'
+        'recipe': make_recipe(),
     }
     return render(request, 'recipes/pages/recipe-view.html', context, status=200)
