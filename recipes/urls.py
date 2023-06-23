0a4b83c019c5fb42ff908651050ec5c4d0dcae26
navas <gabrielnavas@email.com>
2023-06-22 23:17:05 -0300

    configure media url and media root path

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from recipes import views
 
@@ -8,3 +10,8 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('recipes/<int:id>', views.recipe, name="recipe"),
 ]
+
+urlpatterns += static(
+    settings.MEDIA_URL, 
+    document_root=settings.MEDIA_ROOT
+)
