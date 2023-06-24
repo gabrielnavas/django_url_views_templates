b340107dccc4e9b87712d9102f3ce4a2dcf768d4
navas <gabrielnavas@email.com>
2023-06-23 22:02:32 -0300

    refactory recipe could it be nullable

--- recipes/models.py
+++ recipes/models.py
@@ -23,7 +23,7 @@ class Recipe(models.Model):
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%M/%d/', blank=True, default='')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.title
