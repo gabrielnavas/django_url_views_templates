196f373d6470e9f4e7a896403dd7ec77a893e7fd
navas <gabrielnavas@email.com>
2023-06-25 20:51:58 -0300

    recipe by id url test

--- recipes/tests.py
+++ recipes/tests.py
@@ -6,6 +6,11 @@ class RecipeURLsTest(TestCase):
         home_url = reverse('recipes:home')
         self.assertEqual(home_url, '/')
 
+    def test_recipe_by_id_url_is_correct(self):
+        recipe_id = 1
+        home_url = reverse('recipes:recipe', kwargs={'id': recipe_id})
+        self.assertEqual(home_url, f'/recipes/{recipe_id}')
+
     def test_recipe_by_category_url_is_correct(self):
         category_id = 1
         home_url = reverse('recipes:category', kwargs={'id': category_id})
