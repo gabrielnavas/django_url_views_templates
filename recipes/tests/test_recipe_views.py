34a4d5139c9016dc73c07b6f0c60cb5f5b84bdf8
navas <gabrielnavas@email.com>
2023-06-26 16:31:27 -0300

    change functions namess

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -35,12 +35,12 @@ class RecipeViewsTest(TestCase):
         HTTP_CODE_NOT_FOUND = 404
         self.assertEqual(response.status_code, HTTP_CODE_NOT_FOUND) 
 
-    def test_recipe_by_id_view_function_is_correct(self):
+    def test_recipe_detail_view_function_is_correct(self):
         recipe_id = 1
         view = resolve(reverse('recipes:recipe', kwargs={'id': recipe_id}))
         self.assertIs(view.func, views.recipe)
 
-    def test_recipe_view_return_404_if_no_recipes_found(self):
+    def test_recipe_detail_view_return_404_if_no_recipes_found(self):
         recipe_fake_id = 1000
         response = self.client.get(reverse('recipes:recipe', kwargs={'id': recipe_fake_id}))
         HTTP_CODE_NOT_FOUND = 404
