6da0de206600072bc19c6ee6d7fe272833914caa
navas <gabrielnavas@email.com>
2023-06-26 17:22:41 -0300

    testing context of the recipe home template

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from django.urls import reverse, resolve
 
 from recipes import views
+from recipes.models import Category, Recipe
+from django.contrib.auth.models import User
 
 class RecipeViewsTest(TestCase):
     def test_recipe_home_view_function_is_correct(self):
@@ -23,6 +25,45 @@ class RecipeViewsTest(TestCase):
             '<h1>No recipes found here.</h1>', 
             response.content.decode('utf-8')
         )
+
+    def test_recipe_home_template_loads_recipes(self):
+        category = Category.objects.create(name='any_fake_category')
+        author = User.objects.create_user(
+            username='any_username', 
+            email='any_email@email.com', 
+            password='any_password',
+            first_name='any_name',
+            last_name='any_last_name'
+        )
+        Recipe.objects.create(
+            title = 'any_title',
+            description = 'any_description',
+            slug = 'any-slug',
+            preparation_time = 10,
+            preparation_time_unit = 'Minutes',
+            servings = 5,
+            servings_unit = 'Unit',
+            preparation_steps = 'any_steps',
+            preparation_steps_is_html = False,
+            is_published = True,
+            category=category,
+            author=author,
+        )
+        response = self.client.get(reverse('recipes:home'))
+        response_recipes = response.context['recipes'].first()
+        
+        self.assertEqual(response_recipes.title, 'any_title')
+        self.assertEqual(response_recipes.description, 'any_description')
+        self.assertEqual(response_recipes.slug, 'any-slug')
+        self.assertEqual(response_recipes.preparation_time, 10)
+        self.assertEqual(response_recipes.preparation_time_unit, 'Minutes')
+        self.assertEqual(response_recipes.servings, 5)
+        self.assertEqual(response_recipes.servings_unit, 'Unit')
+        self.assertEqual(response_recipes.preparation_steps, 'any_steps')
+        self.assertEqual(response_recipes.preparation_steps_is_html, False)
+        self.assertEqual(response_recipes.is_published, True)
+        self.assertEqual(response_recipes.category.name, category.name)
+        self.assertEqual(response_recipes.author.username, author.username)
 
     def test_recipe_category_view_function_is_correct(self):
         category_id = 1
